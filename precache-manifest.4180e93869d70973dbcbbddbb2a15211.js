e6909c06015e186e98393e540e11b3aba2bfa7e0
Nolan Terpstra <nbakert@gmail.com>
2020-10-02 21:03:39 -0500

    Updates

---
+++ precache-manifest.4180e93869d70973dbcbbddbb2a15211.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5b2b50ee604b2b3e6bd1d88e8930446",
+    "url": "/react-employee-directory/index.html"
+  },
+  {
+    "revision": "84a6d62aa44b870bb413",
+    "url": "/react-employee-directory/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "e80ec9daca50a07c9efd",
+    "url": "/react-employee-directory/static/css/main.d0c2c2bf.chunk.css"
+  },
+  {
+    "revision": "84a6d62aa44b870bb413",
+    "url": "/react-employee-directory/static/js/2.e4d9c60f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-employee-directory/static/js/2.e4d9c60f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e80ec9daca50a07c9efd",
+    "url": "/react-employee-directory/static/js/main.7f3ce90f.chunk.js"
+  },
+  {
+    "revision": "193cdcbd01f85fb428f8",
+    "url": "/react-employee-directory/static/js/runtime-main.76415422.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.0f22c59f63237a0f6b7d25d5ec9c8913.js"
+  "/react-employee-directory/precache-manifest.4180e93869d70973dbcbbddbb2a15211.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-employee-directory/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
